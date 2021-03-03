5d846be3fbacfe6f8816c07d7ec3a38ae986cbfb
OlegShir <63945818+OlegShir@users.noreply.github.com>
2021-03-03 18:36:04 +0300

    1

--- original/bot.py
+++ original/bot.py
@@ -1,7 +1,8 @@
 import logging
 import asyncio
-from datetime import datetime
-
+import emoji
+from aiogram.types import ParseMode, InputMediaPhoto, InputMediaVideo, ChatActions
+from aiogram.utils.markdown import text, bold, italic, code, pre
 from aiogram import Bot, Dispatcher, executor, types
 
 # задаем уровень логов
@@ -11,43 +12,23 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token='1535163103:AAG-Xv6Wki6pT878EPe6xNapmWoQ8jeL5n8')
 dp = Dispatcher(bot)
 
+# иницилизация картинок эмодзи в описании события 
+price = emoji.emojize(":moneybag: ", use_aliases=True)
+metro = emoji.emojize(":metro: м. ", use_aliases=True)
+adress = emoji.emojize(":world_map: ", use_aliases=True)
+raiting = emoji.emojize(":star: :", use_aliases=True)
+link = emoji.emojize(":link: ", use_aliases=True)
 
-'''
-# Команда активации подписки
-@dp.message_handler(commands=['subscribe'])
-async def subscribe(message: types.Message):
-	if(not db.subscriber_exists(message.from_user.id)):
-		# если юзера нет в базе, добавляем его
-		db.add_subscriber(message.from_user.id)
-	else:
-		# если он уже есть, то просто обновляем ему статус подписки
-		db.update_subscription(message.from_user.id, True)
-	
-	await message.answer("Вы успешно подписались на рассылку!\nЖдите, скоро выйдут новые обзоры и вы узнаете о них первыми =)")
+link_event = 'https://speterburg.biglion.ru/deals/spa-programma-vibiray2-54/'
+title_event = 'SPA-программа «Май», «Пляж», «Сила камня», «Восточная сказка», «Сладкие грезы», «Тайский шепот» в салоне Simmetria SPA'
 
-# Команда отписки
-@dp.message_handler(commands=['unsubscribe'])
-async def unsubscribe(message: types.Message):
-	if(not db.subscriber_exists(message.from_user.id)):
-		# если юзера нет в базе, добавляем его с неактивной подпиской (запоминаем)
-		db.add_subscriber(message.from_user.id, False)
-		await message.answer("Вы итак не подписаны.")
-	else:
-		# если он уже есть, то просто обновляем ему статус подписки
-		db.update_subscription(message.from_user.id, False)
-		await message.answer("Вы успешно отписаны от рассылки.")
-'''
+@dp.message_handler()
+async def echo(message: types.Message):
+	print(message)
+	with open('original/a.jpg', 'rb') as photo:
+		await message.answer_photo(photo, caption=bold(title_event) + '\n\n' + \
+			price + '1800'+'\n' + metro + 'Просвещение'+'\n'+ adress +'Просвещение 23'+'\n' + \
+			link + link_event, parse_mode=ParseMode.MARKDOWN)
 
-# проверяем наличие новых игр и делаем рассылки
-bot.send_photo(chat_id,
-			   photo,
-			   caption = nfo['title'] + "\n" + "Оценка: " + nfo['score'] + "\n" + nfo['excerpt'] + "\n\n" + nfo['link'],
-			   disable_notification = True
-			   )
-				
-
-'''
-# запускаем лонг поллинг
 if __name__ == '__main__':
-	dp.loop.create_task(scheduled(10)) # пока что оставим 10 секунд (в качестве теста)
-	executor.start_polling(dp, skip_updates=True)'''
+	executor.start_polling(dp, skip_updates=True)
