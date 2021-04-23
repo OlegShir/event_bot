d32d6134552df946a879a45b146ebd75bfde2e97
OlegShir <63945818+OlegShir@users.noreply.github.com>
2021-04-23 12:47:32 +0300

    1

--- beauty_caption.py
+++ beauty_caption.py
@@ -2,12 +2,21 @@
 #(номер, тип события, ссылка на картинку, заголовок, дата начала, дата оканчания, цена, адрес, метро, ссылка) 
 
 import constant
+import re
+from geocoderMetro import geocoderMetro
+
+g_m = geocoderMetro()
 
 class BeautyCaption:
 
     def __init__(self, event):
         self.event = event
         self.caption = ''
+
+    def delete_spec_symbol(self, string):
+        if string: new_string = re.sub(r'\"', '', string)
+        else:  new_string  = string
+        return new_string
     
     def get_caption(self):
         # выбор значка эмодзи для события
@@ -28,10 +37,13 @@ class BeautyCaption:
         price = self.event[6] if self.event[6] else 'подродности в описании'
         
         # если отсутствует адрес
-        address = f'{constant.address} <a href=\"https://maps.google.com/?q={self.event[7]}\">{self.event[7]}</a>\n' if self.event[7] else ''
+        address_for_link = self.delete_spec_symbol(self.event[7])
+        address = f'{constant.address} <a href=\"https://maps.google.com/?q={address_for_link}\">{self.event[7]}</a>\n' if {self.event[7]} else ''
 
         # если отсутствует метро
-        metro = f'{constant.metro} {self.event[8]}\n' if self.event[8] else ''
+        if not self.event[8]:
+            metro_station = g_m.get_metro(address_for_link)
+        metro = f'{constant.metro}{metro_station}\n' if metro_station else ''
                 
         link =  f'{constant.link} {self.event[9]}'
         
@@ -40,9 +52,9 @@ class BeautyCaption:
         return caption
 
 #
-'''
+
 if __name__ == '__main__':
-    event = (191648, 'Отдых', 'https://kudago.com/media/thumbs/640x384/images/event/5d/6f/5d6fe7d5640cd4a513c4d108c3dfb4fe.jpg', 'забеги Parkrun', None, None, '', 0, None, None, 'https://kudago.com/spb/event/recreation-zabegi-parkrun/')
+    event = ('37265', 'Обучение', 'https://peterburg.center/sites/default/files/img/event_m/2021-03/len-zoo.jpg', 'Библионочь-2021: От зверинца – к зоопарку', '24 апреля 2021', None, None, 'Никольская пл., 2 (Библиотека "Старая Коломна")', None, 'https://peterburg.center/event/biblionoch-2021-ot-zverinca-k-zooparku.html')
     bc = BeautyCaption(event)
     caption = bc.get_caption()
-    print(caption)'''
+    print(caption)
